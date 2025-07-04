1937a0f4da23495a21bcc5529fd4730ff5d1aac2
webbiefpv <webbiefpv@gmail.com>
2025-07-04 23:42:04 +0000

    full form truly dynamic fields

--- setup_form.php
+++ setup_form.php
@@ -108,17 +108,101 @@ function render_field($field_name, $input_name, $saved_value, $options_by_catego
 <?php require 'header.php'; ?>
 <div class="container mt-3">
     <h1>Setup: <?php echo htmlspecialchars($setup['name']); ?></h1>
-    <?php if(isset($_GET['msg'])) echo '<div class="alert alert-danger">'.htmlspecialchars($_GET['msg']).'</div>'; ?>
     
+    <?php if(isset($_GET['success'])) echo '<div class="alert alert-success">Setup saved successfully!</div>'; ?>
+    <?php if(isset($_GET['error'])) echo '<div class="alert alert-danger">An error occurred while saving. Please check your data.</div>'; ?>
+    <?php if(isset($_GET['msg'])) echo '<div class="alert alert-danger">'.htmlspecialchars($_GET['msg']).'</div>'; ?>
+
     <form method="POST">
+        <h3>Front Suspension</h3>
+        <div class="row">
+        <?php
+            $fields = ['ackermann', 'arms', 'bumpsteer', 'droop_shims', 'kingpin_fluid', 'ride_height', 'arm_shims', 'springs', 'steering_blocks', 'steering_limiter', 'track_wheel_shims'];
+            foreach ($fields as $field) {
+                render_field($field, 'front_suspension[' . $field . ']', $data['front_suspension'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+
+        <h3>Rear Suspension</h3>
+        <div class="row">
+        <?php
+            $fields = ['axle_height', 'centre_pivot_ball', 'centre_pivot_fluid', 'droop', 'rear_pod_shims', 'rear_spring', 'ride_height', 'side_bands_lr'];
+            foreach ($fields as $field) {
+                render_field($field, 'rear_suspension[' . $field . ']', $data['rear_suspension'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+
+        <h3>Front Tires</h3>
+        <div class="row">
+        <?php
+            $fields = ['tire_brand', 'tire_compound', 'wheel_brand_type', 'tire_additive', 'tire_additive_area', 'tire_additive_time', 'tire_diameter', 'tire_side_wall_glue'];
+            foreach ($fields as $field) {
+                render_field($field, 'tires_front[' . $field . ']', $data['tires_front'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+
+        <h3>Rear Tires</h3>
+        <div class="row">
+        <?php
+            $fields = ['tire_brand', 'tire_compound', 'wheel_brand_type', 'tire_additive', 'tire_additive_area', 'tire_additive_time', 'tire_diameter', 'tire_side_wall_glue'];
+            foreach ($fields as $field) {
+                render_field($field, 'tires_rear[' . $field . ']', $data['tires_rear'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+        
+        <h3>Drivetrain</h3>
+        <div class="row">
+        <?php
+            $fields = ['axle_type', 'drive_ratio', 'gear_pitch', 'rollout', 'spur'];
+            foreach ($fields as $field) {
+                render_field($field, 'drivetrain[' . $field . ']', $data['drivetrain'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+
+        <h3>Body and Chassis</h3>
+        <div class="row">
+        <?php
+            $fields = ['battery_position', 'body', 'body_mounting', 'chassis', 'electronics_layout', 'motor_position', 'motor_shims', 'rear_wing', 'screw_turn_buckles', 'servo_position', 'weight_balance_fr', 'weight_total', 'weights'];
+            foreach ($fields as $field) {
+                render_field($field, 'body_chassis[' . $field . ']', $data['body_chassis'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+
         <h3>Electronics</h3>
         <div class="row">
         <?php
-            $electronics_fields = ['battery_brand', 'esc_brand', 'motor_brand', 'radio_brand', 'servo_brand', 'battery', 'battery_c_rating', 'capacity', 'model', 'esc_model', 'motor_kv_constant', 'motor_model', 'motor_timing', 'motor_wind', 'radio_model', 'receiver_model', 'servo_model', 'transponder_id'];
-            foreach ($electronics_fields as $field) {
+            $fields = ['battery_brand', 'esc_brand', 'motor_brand', 'radio_brand', 'servo_brand', 'battery', 'battery_c_rating', 'capacity', 'model', 'esc_model', 'motor_kv_constant', 'motor_model', 'motor_timing', 'motor_wind', 'radio_model', 'receiver_model', 'servo_model', 'transponder_id'];
+            foreach ($fields as $field) {
                 render_field($field, 'electronics[' . $field . ']', $data['electronics'][$field] ?? null, $options_by_category);
             }
         ?>
+        </div>
+
+        <h3>ESC Settings</h3>
+        <div class="row">
+        <?php
+            $fields = ['boost_activation', 'boost_rpm_end', 'boost_rpm_start', 'boost_ramp', 'boost_timing', 'brake_curve', 'brake_drag', 'brake_frequency', 'brake_initial_strength', 'race_mode', 'throttle_curve', 'throttle_frequency', 'throttle_initial_strength', 'throttle_neutral_range', 'throttle_strength', 'turbo_activation_method', 'turbo_delay', 'turbo_ramp', 'turbo_timing'];
+            foreach ($fields as $field) {
+                render_field($field, 'esc_settings[' . $field . ']', $data['esc_settings'][$field] ?? null, $options_by_category);
+            }
+        ?>
+        </div>
+
+        <div class="row">
+            <div class="col-12 mb-3">
+                <label class="form-label">Notes</label>
+                <textarea class="form-control" name="front_suspension[notes]"><?php echo htmlspecialchars($data['front_suspension']['notes'] ?? ''); ?></textarea>
+            </div>
+            <div class="col-12 mb-3">
+                <label class="form-label">Comments</label>
+                <textarea class="form-control" name="comments[comment]"><?php echo htmlspecialchars($data['comments']['comment'] ?? ''); ?></textarea>
+            </div>
         </div>
         
         <hr>
